7b0938b296bd22f20794c334375897af7e2db9b0
111 <111@222.com>
2025-11-28 10:45:46 +0800

    ccc

--- index.5ad09.js
+++ index.5ad09.js
@@ -1,4 +1,4 @@
-System.register(["./application.ea0b9.js"], function (_export, _context) {
+System.register(["./application.24ace.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
