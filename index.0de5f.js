6c1724f25ed85220b71c2385af0a9bdb05162752
111 <111@222.com>
2025-11-28 10:33:45 +0800

    fff

--- index.0de5f.js
+++ index.0de5f.js
@@ -1,4 +1,4 @@
-System.register(["./application.4c4fa.js"], function (_export, _context) {
+System.register(["./application.4f71b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
