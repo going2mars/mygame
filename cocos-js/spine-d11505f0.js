b33c1e85ce5d8a0b73eca84679900a0ae5e5ed78
111 <111@222.com>
2025-11-28 10:35:29 +0800

    bbb

--- cocos-js/spine-d11505f0.js
+++ cocos-js/spine-d11505f0.js
@@ -1,11 +1,1 @@
-System.register([], (function (exports, module) {
-	'use strict';
-	return {
-		execute: (function () {
-
-			var spine = exports('default', 'assets/spine-17c81aa0.wasm'); /* asset-hash:debad078 */
-
-		})
-	};
-}));
-//# sourceMappingURL=spine-d11505f0.js.map
+System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-17c81aa0.wasm")}}}));
